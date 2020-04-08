52eb1d60cb9228caa4a5bb5ba65a4703efbe733d
Kazzila <mrkazila@yandex.ru>
2020-04-08 12:33:41 +0300

    initial commit

---
+++ PageObjectStepik/Section4/pages/main_page.py
@@ -0,0 +1,7 @@
+from .base_page import BasePage # импорт базового класса BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage): # класс MAinPAge наследник класса BasePage. Класс-предок в Python указывается в скобках
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link") # Так как браузер у нас хранится как аргумент класса BasePage, обращаться к нему нужно соответствующим образом с помощью self
+        login_link.click() #
