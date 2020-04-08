4a45c1a64cd9f4696f67a3d7ff6d89a790c9a7a3
Kazzila <mrkazila@yandex.ru>
2020-04-08 12:58:42 +0300

    update error massage

--- PageObjectStepik/Section4/pages/base_page.py
+++ PageObjectStepik/Section4/pages/base_page.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import NoSuchElementException # Чтобы импортировать нужное нам исключение, в самом верху файла нужно указать
+
 class BasePage():
     '''
     Теперь в наш класс нужно добавить методы. Первым делом добавим конструктор — метод, который вызывается,
@@ -5,11 +7,24 @@ class BasePage():
     В него в качестве параметров мы передаем экземпляр драйвера и url адрес.
     Внутри конструктора сохраняем эти данные как аттрибуты нашего класса. Получается примерно так:
     '''
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
     '''
     Теперь добавим еще один метод open. Он должен открывать нужную страницу в браузере, используя метод get()
     '''
     def open(self):
         self.browser.get(self.url)
+
+'''    
+Теперь в этом же классе реализуем метод is_element_present, в котором будем перехватывать исключение.В него будем
+передавать два аргумента: как искать(css, id, xpath и тд) и собственно что искать(строку - селектор).
+Чтобы перехватывать исключение, нужна конструкция try/ except
+'''
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- PageObjectStepik/Section4/pages/main_page.py
+++ PageObjectStepik/Section4/pages/main_page.py
@@ -2,6 +2,9 @@ from .base_page import BasePage # импорт базового класса Bas
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): # класс MAinPAge наследник класса BasePage. Класс-предок в Python указывается в скобках
-    def go_to_login_page(self):
+    '''def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link") # Так как браузер у нас хранится как аргумент класса BasePage, обращаться к нему нужно соответствующим образом с помощью self
-        login_link.click() #
+        login_link.click() #'''
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
